a448228c13642094ce2f3f897b252d833902ac80
Eric Guo /Peng <errrrrricg@gmail.com>
2022-12-29 14:00:48 -0500

    corpse implemented. part 2 finished implementation

--- Assets/Scripts/UpGroundObj/Pickable.cs
+++ Assets/Scripts/UpGroundObj/Pickable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Pickable : UpGroundObj
 {
     [SerializeField] GameObject objProduce;
+    [SerializeField] int produceAmount = 1;
     [SerializeField] ItemScriptableObject isoToGet;
     // Start is called before the first frame update
 
@@ -12,10 +13,28 @@ public class Pickable : UpGroundObj
     {
         if (objProduce != null)
         {
+            StartCoroutine(PickUpIE());
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator PickUpIE()
+    {
+        int amount = 0;
+        while(amount < produceAmount)
+        {
             GameObject ngo = Instantiate(objProduce, transform.parent);
             ngo.transform.position = transform.position;
             ngo.GetComponent<mushroom>().SetUp(GetComponent<SpriteRenderer>().sprite, isoToGet);
+            amount += 1;
+            if(amount == produceAmount)
+            {
+                Destroy(gameObject);
+            }
+            yield return new WaitForSeconds(0.3f);
         }
-        Destroy(gameObject);
     }
 }
